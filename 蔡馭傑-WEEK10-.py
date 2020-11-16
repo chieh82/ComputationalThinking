5fd6d1fe921a35863a2751c7f8f725e270f24286
chieh82 <71250961+chieh82@users.noreply.github.com>
2020-11-16 14:54:28 +0800

    Add files via upload

---
+++ 蔡馭傑-WEEK10-.py
@@ -0,0 +1,36 @@
+#1. 變數賦值
+X=1
+
+##用等號=讓電腦給記憶空間
+##等號左邊
+##等號右邊
+
+##有什麼用處?
+
+X=2
+Y=5
+
+print(X+Y)
+print(X-Y)
+print(X*Y)
+print(X//Y)
+print(X%Y)
+print(X**Y)
+
+magic_number=22**3+17//5+17%5
+print(magic_number)
+
+#4.電腦輸出
+##print()函數
+print("神奇數字為",magic_number)
+
+#3.電腦輸入
+##input("Hint")函數
+###文字資料
+num=input("Enter a number?")
+num=int(num)
+num=num+1
+print(num)
+
+
+
